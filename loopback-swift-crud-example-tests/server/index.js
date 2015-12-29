b389fd39a052eab3b5acbd0655492c52232f001c
Kevin Gödecke <goedecke@net-studios.de>
2015-12-29 14:32:07 +0100

    Added LoopBack Test App

---
+++ loopback-swift-crud-example-tests/server/index.js
@@ -0,0 +1,99 @@
+var loopback = require('loopback');
+var path = require('path');
+var app = loopback();
+app.set('legacyExplorer', false);
+//app.use(loopback.logger(app.get('env') === 'development' ? 'dev' : 'default'));
+app.dataSource('Memory', {
+  connector: loopback.Memory,
+  defaultForType: 'db'
+});
+
+var Widget = app.model('widget', {
+  properties: {
+    name: {
+      type: String,
+      required: true
+    },
+    bars: {
+      type: Number,
+      required: false
+    },
+    bars2: {
+      type: Number,
+      required: false
+    },
+    flag: {
+      type: Boolean,
+      required: false
+    },
+    flag2: {
+      type: Boolean,
+      required: false
+    },
+    date: {
+      type: Date,
+      required: false
+    },
+    data: {
+      type: Object,
+      required: false
+    }
+  },
+  dataSource: 'Memory'
+});
+
+var lbpn = require('loopback-component-push');
+var PushModel = lbpn.createPushModel(app, { dataSource: app.dataSources.Memory });
+var Installation = lbpn.Installation;
+Installation.attachTo(app.dataSources.Memory);
+app.model(Installation);
+
+var ds = app.dataSource('storage', {
+  connector: require('loopback-component-storage'),
+  provider: 'filesystem',
+  root: path.join(__dirname, 'storage')
+});
+
+var container = loopback.createModel({ name: 'container', base: 'Model' });
+app.model(container, { dataSource: 'storage' });
+
+Widget.destroyAll(function () {
+  Widget.create({
+    name: 'Foo',
+    bars: 0,
+    data: {
+      quux: true
+    }
+  });
+  Widget.create({
+    name: 'Bar',
+    bars: 1,
+    date: '2000-01-02T03:04:05.006Z'
+  });
+});
+
+app.model(loopback.AccessToken);
+
+app.model('Customer', {
+  options: {
+    base: 'User',
+    relations: {
+      accessTokens: {
+        model: "AccessToken",
+        type: "hasMany",
+        foreignKey: "userId"
+      }
+    }
+  },
+  dataSource: 'Memory'
+});
+
+app.dataSource('mail', { connector: 'mail', defaultForType: 'mail' });
+loopback.autoAttach();
+
+app.enableAuth();
+app.use(loopback.token({ model: app.models.AccessToken }));
+app.use(loopback.rest());
+app.listen(3000, function() {
+  console.log('https server is ready at https://localhost:3000.');
+});
